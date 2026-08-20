b9f41655b15d3e5d07337e99de6496086a55bac8
Sajjad Haider <sajjadhaider1016@gmail.com>
2026-08-20 16:23:48 +0500

    Add passwordHash migration

---
+++ prisma/migrations/20260820112153_rename_password_to_password_hash/migration.sql
@@ -0,0 +1,42 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Memory` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the column `password` on the `User` table. All the data in the column will be lost.
+  - Added the required column `passwordHash` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "Conversation_userId_idx";
+
+-- DropIndex
+DROP INDEX "Memory_userId_key_key";
+
+-- DropIndex
+DROP INDEX "Memory_userId_idx";
+
+-- DropIndex
+DROP INDEX "Message_conversationId_idx";
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Memory";
+PRAGMA foreign_keys=on;
+
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_User" ("createdAt", "email", "id", "name", "updatedAt") SELECT "createdAt", "email", "id", "name", "updatedAt" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
